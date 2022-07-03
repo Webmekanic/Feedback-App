9c64790cfb1c4028badaa5d4787de9dc0ab53f31
aniebodavid <aniebovictor001@gmail.com>
2022-07-03 06:14:32 -0700

    initialize feedback app

---
+++ src/components/feedbackItem.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react"
+
+function FeedbackItem() {
+  const [rating, setRating] = useState(7)
+  const [text, setText] = useState("This is an example of feedback")
+  const handleClick = (e) => {
+    setRating((prev) => {
+      console.log(prev)
+      return prev + 1
+    })
+  }
+  return (
+    <div className="card">
+      <div className="num-display">{rating}</div>
+      <div className="text-display">{text}</div>
+      <button onClick={handleClick}>Click</button>
+    </div>
+  )
+}
+
+export default FeedbackItem
