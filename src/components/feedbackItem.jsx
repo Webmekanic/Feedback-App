920b6a8403e5a2911a499781b857f94011570790
aniebodavid <aniebovictor001@gmail.com>
2022-07-04 10:19:46 -0700

    feat:: add events and events drilling

--- src/components/feedbackItem.jsx
+++ src/components/feedbackItem.jsx
@@ -1,11 +1,15 @@
 import React from "react"
 import Card from "./shared/card"
 import PropTypes from "prop-types"
+import { FaTimes } from "react-icons/fa"
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button className="close">
+        <FaTimes onClick={() => handleDelete(item.id)} color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   )
